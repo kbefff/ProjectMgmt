2df3d74e18ae4341b7e0ced4e04714ea622bea46
kbefff <kbeffel10@me.com>
2023-12-28 15:37:19 -0800

    use graphiql when in development, add .gitignore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express'); // yarn add express
 require('dotenv').config()
-const {graphqlHTTP} = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
 // const { createHandler } = require('graphql-http/lib/use/express');
 const schema = require('./schema/schema');
 const port = process.env.PORT || 8080
@@ -9,15 +9,10 @@ const app = express();
 app.use(
     '/graphql',
     graphqlHTTP({
-      schema: schema,
-      graphiql: true,
+        schema: schema,
+        graphiql: process.env.NODE_ENV === 'development',
     }),
-  );
-// app.use('/graphql', graphqlHTTP({
-//     schema: schema,
-//     graphiql: process.env.NODE_ENV === 'development',
-// }));
-// app.all('/graphql', createHandler({ schema, graphiql: true }));
+);
 
 console.log(`running in ${process.env.NODE_ENV || "default"} mode`)
 app.listen(port, console.log(`Listening to http://localhost:${port}`));
