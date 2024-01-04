181d07830de84d52885b0f5816d993d6a6971c86
kbefff <kbeffel10@me.com>
2024-01-04 10:47:33 -0800

    adjustments for production mode static build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
     return (
         <>
             <div className="d-flex gap-3 mb-4">
-                <AddClientModal/>
-                <AddProjectModal/>
+                <AddClientModal />
+                <AddProjectModal />
 
             </div>
             <Projects />
