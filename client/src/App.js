64d4726f6412d79e3da7be486ac0e7101b0e05ea
kbefff <kbeffel10@me.com>
2024-01-13 16:04:51 -0800

    update apollo client uri for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-
+import dotenv from 'dotenv';
 import Home from "./pages/Home";
 import Project from "./pages/Project";
 import NotFound from "./pages/NotFound";
-
 import Header from "./components/Header";
+
+dotenv.config();
 
 const port = 8080;
 
 const cache = new InMemoryCache({
-  typePolicies: { // handle merge of incoming data with existing data (ex: when adding a new client, we want to merge the new client with the existing clients)
+  typePolicies: {
     Query: {
       fields: {
         clients: {
@@ -27,10 +28,13 @@ const cache = new InMemoryCache({
       },
     },
   }
-}
-);
+});
 
-const client = new ApolloClient({ uri: `http://localhost:${port}/graphql`, cache });
+console.log(process.env.NODE_ENV);
+console.log(process.env.MONGO_URI);
+console.log(process.env.GRAPHQL_URI);
+
+const client = new ApolloClient({ uri: process.env.GRAPHQL_URI || "https://project-mgmt-72ab7f289f44.herokuapp.com/graphql", cache });
 
 function App() {
   return (
@@ -39,16 +43,15 @@ function App() {
         <Router>
           <Header />
           <div className="container">
-           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects/:id" element={<Project/>} />
-            <Route path='*' element={<NotFound />} />
-           </Routes>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
           </div>
         </Router>
       </ApolloProvider>
     </>
-
   );
 }
 
